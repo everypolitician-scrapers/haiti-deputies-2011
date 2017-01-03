e4f59f3a8d48420167cf557a74b7b8beaa01b047
Tony Bowden <tony@tmtm.com>
2017-01-03 23:11:41 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'nokogiri'
@@ -12,7 +13,7 @@ OpenURI::Cache.cache_path = '.cache'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
@@ -28,28 +29,28 @@ def scrape_list(url)
       tds = tr.css('td')
 
       circ = tds[1].text.tidy.sub('unique circ', '1è circ').sub('circ. unique', '1è circ')
-      cap = circ.match(/(.*):\s*(\d+)è\.?\s*circ\.?\s*(.*)/) or raise "Can't parse area: #{tds[1].text}"
+      (cap = circ.match(/(.*):\s*(\d+)è\.?\s*circ\.?\s*(.*)/)) || raise("Can't parse area: #{tds[1].text}")
 
-      area = { 
-        departement: dep.text.tidy.sub('DÉPARTEMENT ','').sub(/^(du|de l.)\s*/, ''),
-        district: cap[1],
-        circ_id: cap[2],
-        circ: cap[3],
+      area = {
+        departement: dep.text.tidy.sub('DÉPARTEMENT ', '').sub(/^(du|de l.)\s*/, ''),
+        district:    cap[1],
+        circ_id:     cap[2],
+        circ:        cap[3],
       }
       area[:district] = 'Saint Marc' if area[:district] == 'St. Marc'
-      area[:id] = 'ocd-division/country:ht/departement:%s/arrondissement:%s/circonscription:%s' % 
-        %i(departement district circ_id).map { |i| area[i].downcase.tr(' ','_') }
+      area[:id] = 'ocd-division/country:ht/departement:%s/arrondissement:%s/circonscription:%s' %
+                  %i(departement district circ_id).map { |i| area[i].downcase.tr(' ', '_') }
 
-      data = { 
-        name: tds[0].text.tidy.sub('Siège vacant dû au décès de ',''),
-        region: dep.text.tidy,
+      data = {
+        name:    tds[0].text.tidy.sub('Siège vacant dû au décès de ', ''),
+        region:  dep.text.tidy,
         area_id: area[:id],
-        area: area[:circ],
-        party: tds[2].text.tidy,
-        term: '2011',
-        source: url,
+        area:    area[:circ],
+        party:   tds[2].text.tidy,
+        term:    '2011',
+        source:  url,
       }
-      ScraperWiki.save_sqlite([:name, :area_id], data)
+      ScraperWiki.save_sqlite(%i(name area_id), data)
     end
   end
 end
